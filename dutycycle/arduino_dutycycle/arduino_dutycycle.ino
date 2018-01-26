f9bf048184375db07f807f9ad03ba7e1a5a4c1d6
Joel <joel@redpill-linpro.com>
2018-01-26 21:14:00 +0100

    Use analog pins for controlling LEDs

--- dutycycle/arduino_dutycycle/arduino_dutycycle.ino
+++ dutycycle/arduino_dutycycle/arduino_dutycycle.ino
@@ -1,25 +1,22 @@
-const int PIN_READ_POT_PERCENTAGE = 0;
-const int PIN_READ_SHIFT_RELAYS = 10;
-const int PIN_READ_MASTER_SWITCH = 9;
-const int MAX_WATTAGE = 9900;
+const int PIN_READ_POT_PERCENTAGE = A0;
+const int PIN_READ_SHIFT_RELAYS = 2;
+const int PIN_READ_MASTER_SWITCH = A5;
 
 const int SHIFT_RELEYS_INTERVAL = 1;
 const int MIN_FLICKER_LENGTH = 20; // one cycle at 50Hz is 10ms,
+const int MIN_ON_TIME = 350; // Set to at least min flicker time.
 const boolean START_ACTIVE_STATE = true;
 
-// States of the relays
-boolean relays[3] = {LOW, LOW, LOW};
 // Is set in shiftRelays function
-int relay_order[3] = {0, 1, 2};
+const int RELAY_PINS[3] = {A1, A2, A4};
+int relays[3] = {RELAY_PINS[0], RELAY_PINS[1], RELAY_PINS[2]};
 
-int calculate_wattage_output;
 int val;
 int percentage;
 int window_length = 500;
 int onTime_distribution[3] = {0, 0, 0};
 // millisecond, max is window_length;
 int frame;
-float duty_cycle_factor;
 
 // What pid is currently selected 0 = no pid, 1 = 1, 2 = 2;
 int selected_pid;
@@ -29,9 +26,12 @@ int loop_counter = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  for(int i = 0; i < 3; i ++){
-    pinMode(i + 2, OUTPUT);
-  }
+  
+  pinMode(relays[0], OUTPUT);
+  pinMode(relays[1], OUTPUT);
+  pinMode(relays[2], OUTPUT);
+  pinMode(PIN_READ_MASTER_SWITCH, INPUT);
+  pinMode(PIN_READ_SHIFT_RELAYS, INPUT);
   
   Serial.begin(9600);
 }
@@ -41,55 +41,25 @@ void shiftRelays(){
   if(!digitalRead(PIN_READ_SHIFT_RELAYS)){
     for(int i = 0; i < 3; i++)
     {
-      relay_order[i] = i;
+      relays[i] = RELAY_PINS[i];
     }
     
     return;
   }
   
-  int first_relay_number = relay_order[0];
+  int first_relay_pin = relays[0];
   
   for(int i = 0; i < 2; i++)
   {
-    relay_order[i] = relay_order[i + 1];
+    relays[i] = relays[i + 1];
   }
-  relay_order[2] = first_relay_number;
+  relays[2] = first_relay_pin;
 }
-
-int getPercentage(){
-  int input = 0;
-  int pot_value = analogRead(PIN_READ_POT_PERCENTAGE);
-  int p;
-  
-  if(pot_value > 0){
-    input = ceil(pot_value);
-  }
-  
-  p = map(
-    input,
-    0,
-    1023,
-    0,
-    100
-  );
-  
-  return p;
-  
-  if(p >= 31 && p <= 34){
-    return 35;
-  }
-  
-  return p;
-}
-
-float getFactor(){
-  return float(getPercentage()) / 100;
-}
-
 
 int getFrame(){
   return millis() % window_length;
 }
+
 void resetOnTimeDistribution(){
   for(int i = 0; i < 3; i++)
   {
@@ -97,8 +67,15 @@ void resetOnTimeDistribution(){
   }
 }
 
+int getMaxOnTime(){
+  return window_length * 3;
+}
+
+int getMinOnTime(){
+  return MIN_ON_TIME;
+}
+
 int getTotalOnTimeToDistribute(){
-  float factor = getFactor();
   int input = 0;
   int pot_value = analogRead(PIN_READ_POT_PERCENTAGE);
   int p;
@@ -106,24 +83,22 @@ int getTotalOnTimeToDistribute(){
   if(pot_value > 0){
     input = ceil(pot_value);
   }
+
+  Serial.println("POT: " + String(input));
   
   return map(
     input,
     0,
-    1023,
-    MIN_FLICKER_LENGTH,
-    window_length * 3
+    1015,
+    getMinOnTime(),
+    getMaxOnTime()
   );
 }
 
 void setOnTimeDistribution(){
-  float factor = getFactor();
   int onTimeToDistribute = getTotalOnTimeToDistribute();
   int onTime;
   
-  Serial.print("Factor: ");
-  Serial.println(factor);
-
   Serial.println("Total time to distribute: " + String(onTimeToDistribute));
   
   for(int i = 0; i < 3; i++)
@@ -162,10 +137,6 @@ void setOnTimeDistribution(){
 }
 
 void setRelays(){
-  boolean current_relay_state;
-  boolean wanted_relay_state;
-  int relay_index = 0;
-
   // If the master-on-switch is Off, reset on time distribution, should only be set at frame 0 to avoid fast-switching of relays.
   if(!digitalRead(PIN_READ_MASTER_SWITCH) == false){
      resetOnTimeDistribution();
@@ -173,31 +144,21 @@ void setRelays(){
   
   for(int i = 0; i < 3; i++)
   {
-    relay_index = relay_order[i];
-    
-    wanted_relay_state = frame < onTime_distribution[i] ? HIGH : LOW;
-    current_relay_state = relays[relay_index];
-    
-    
-    if(wanted_relay_state != current_relay_state){
-      // Set relay state
-      relays[relay_index] = wanted_relay_state;
-      
-      // Toggle output
-      digitalWrite(relay_index + 2, wanted_relay_state);
-    }
-    /*
-    if(frame < onTime_distribution[i]){
-      digitalWrite(relay_order[i] + 2, HIGH);
-    }else{
-      digitalWrite(relay_order[i] + 2, LOW);
-    }
-    */
+    digitalWrite(
+      relays[i],
+      frame < onTime_distribution[i] ? HIGH : LOW
+    );
   }
 }
 
-void setCalculatedWattageOutput(){
-  calculate_wattage_output = int(ceil(float(MAX_WATTAGE) * duty_cycle_factor));
+int getDisplayOutput(){
+  return map(
+    getTotalOnTimeToDistribute(),
+    getMinOnTime(),
+    getMaxOnTime(),
+    0,
+    9
+  );
 }
 
 // the loop function runs over and over again forever
@@ -218,20 +179,19 @@ void loop() {
     
     setOnTimeDistribution();
     shiftRelays();
-    int percentage = getPercentage();
     
     Serial.println(String(onTime_distribution[0]) + "ms");
     Serial.println(String(onTime_distribution[1]) + "ms");
     Serial.println(String(onTime_distribution[2]) + "ms");
 
 
-    Serial.println("reley order[0]: " + String(relay_order[0]));
-    Serial.println("reley order[1]: " + String(relay_order[1]));
-    Serial.println("reley order[2]: " + String(relay_order[2]));
+    Serial.println("releys[0]: " + String(relays[0]));
+    Serial.println("releys[1]: " + String(relays[1]));
+    Serial.println("releys[2]: " + String(relays[2]));
+
+    Serial.print("Display: ");
+    Serial.println(getDisplayOutput());
     
-    
-    Serial.println(String(calculate_wattage_output) + "w");
-    Serial.print(percentage);
     Serial.print(" - ");
     Serial.print(frame);
     Serial.print(" - ");
